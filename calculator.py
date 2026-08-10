8e145259c482be63041e0c41e3462b83b6b635f9
Ahmadreza-aman2007 <ahmadreza.aman2007@gmail.com>
2026-08-10 12:36:09 +0330

    feat: add exponent and modulo operators and implement exit mechanism

--- calculator.py
+++ calculator.py
@@ -2,35 +2,63 @@ def return_res_format(num1: float, operator: str, num2: float, res: float) -> st
     return f"{num1} {operator} {num2} = {res}"
 
 
-while True:
+def want_to_exit(order) -> bool:
+    if order.strip() == "q":
+        return True
+    return False
+
+
+def main() -> None:
+    print("if you want to exit the program enter q")
     while True:
-        try:
-            num1 = float(input("enter the first number: ").strip())
-            break
-        except ValueError:
-            print("please enter a real number")
-    while True:
-        op = input("please enter a operator:(like:{+,-,*,/}) ").strip()
-        if op not in ["+", "-", "*", "/"]:
-            print("please enter a valid operator")
-        else:
-            break
-    while True:
-        try:
-            num2 = float(input("enter the second number: ").strip())
-            break
-        except ValueError:
-            print("please enter a real number")
-    if op == "-":
-        print(return_res_format(num1, op, num2, num1 - num2))
-    elif op == "+":
-        print(return_res_format(num1, op, num2, num1 + num2))
-    elif op == "*":
-        print(return_res_format(num1, op, num2, num1 * num2))
-    else:
-        if num2 == 0:
-            print("second number can't be zero")
-            continue
-        else:
-            print(return_res_format(num1, op, num2, num1 / num2))
-        break
+        while True:
+            try:
+                order = input(
+                    'enter the first number:("if you want to exit the program enter q") '
+                ).strip()
+                if want_to_exit(order):
+                    return
+                num1 = float(order)
+                break
+            except ValueError:
+                print("please enter a real number")
+        while True:
+            op = input(
+                'please enter a operator:(like:{+,-,*,/,^,%})("if you want to exit the program enter q") '
+            ).strip()
+            if want_to_exit(op):
+                return
+            if op not in ["+", "-", "*", "/", "^", "%"]:
+                print("please enter a valid operator")
+            else:
+                break
+        while True:
+            order = input(
+                'enter the second number:("if you want to exit the program enter q") '
+            ).strip()
+            if want_to_exit(order):
+                return
+            try:
+                num2 = float(order)
+                break
+            except ValueError:
+                print("please enter a real number")
+        if op == "-":
+            print(return_res_format(num1, op, num2, num1 - num2))
+        elif op == "+":
+            print(return_res_format(num1, op, num2, num1 + num2))
+        elif op == "*":
+            print(return_res_format(num1, op, num2, num1 * num2))
+        elif op == "/":
+            if num2 == 0:
+                print("second number can't be zero")
+            else:
+                print(return_res_format(num1, op, num2, num1 / num2))
+        elif op == "^":
+            print(return_res_format(num1, op, num2, num1**num2))
+        elif op == "%":
+            print(return_res_format(num1, op, num2, num1 % num2))
+
+
+if __name__ == "__main__":
+    main()
